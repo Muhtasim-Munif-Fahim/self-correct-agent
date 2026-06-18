48a176f5251fb7f7d0294c2c6ecb0c19a715059d
Muhtasim-Munif-Fahim <s1911024120@ru.ac.bd>
2026-06-19 01:21:46 +0600

    chore: bump version to 0.2.2

--- src/self_correct/__init__.py
+++ src/self_correct/__init__.py
@@ -3,7 +3,7 @@
 from .core import AntiHallucinator, AntiHallucinationResponse, TokenUsage
 from .tools import DuckDuckGoSearchTool, SearchResult, StaticKnowledgeTool, Tool, WikipediaSearchTool
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 
 __all__ = [
     "AntiHallucinator",
